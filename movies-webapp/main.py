3e24d27afaa0d358c81c2a1eea7695263d22a69d
Aatmik Dahal <dahalaatmik@gmail.com>
2025-12-30 15:30:01 +1100

    Update main.py

--- movies-webapp/main.py
+++ movies-webapp/main.py
@@ -41,7 +41,7 @@ class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
-    description: Mapped[str] = mapped_column(String(), nullable=False)
+    description: Mapped[str] = mapped_column(String(100000), nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=True)
     ranking: Mapped[int] = mapped_column(Integer, nullable=True)
     review: Mapped[str] = mapped_column(String(1000), nullable=True)
