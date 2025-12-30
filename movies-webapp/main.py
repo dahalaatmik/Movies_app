ce58e98d7d779e1cde1686f121b52a3e52c00ad1
Aatmik Dahal <dahalaatmik@gmail.com>
2025-12-30 15:20:10 +1100

    updated db
    updated db structure and datatype holds

--- movies-webapp/main.py
+++ movies-webapp/main.py
@@ -41,11 +41,11 @@ class Movie(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     year: Mapped[int] = mapped_column(Integer, nullable=False)
-    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    description: Mapped[str] = mapped_column(String(100000), nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=True)
     ranking: Mapped[int] = mapped_column(Integer, nullable=True)
-    review: Mapped[str] = mapped_column(String(250), nullable=True)
-    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+    review: Mapped[str] = mapped_column(String(1000), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(1000), nullable=False)
 
 
 with app.app_context(): 
