d3283b7d8a9fb85e2bc643bb078a906985ac2c31
Aatmik Dahal <dahalaatmik@gmail.com>
2025-12-30 13:10:27 +1100

    Update main.py

--- movies-webapp/main.py
+++ movies-webapp/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for 
-from bootstrap-flask import Bootstrap5
+from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float
